a846b5271cf0781ec6ef07818ae41c6175656f41
Martin Jagielski <martin.jagielski@comdirect.de>
2019-03-26 11:16:34 +0100

    Initial commit

---
+++ Example/FoodTrackerModel/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  FoodTrackerModel
+//
+//  Created by Martin Jagielski on 03/26/2019.
+//  Copyright (c) 2019 Martin Jagielski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
